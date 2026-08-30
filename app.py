8fd7fc22b8cd64b8b4ea5ebd6e2b5e1b7f78c5ea
Harshitha N <harshithandata@gmail.com>
2026-08-30 10:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from gmail_threat_detector import ComprehensiveThreatDetector, ProductionThreatAPI
 
 detector = ComprehensiveThreatDetector()
+detector.initialize_gmail('/etc/secrets/credentials.json')
 api = ProductionThreatAPI(detector)
 
 app = api.app
